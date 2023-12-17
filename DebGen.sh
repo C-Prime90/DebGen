f7460cc61a9976433b9e88c2a87961f3ae0a1f8a
Corey Moyer <cronmod.dev@gmail.com>
2023-12-16 21:32:48 -0500

    DebGen v1.0.1

--- DebGen.sh
+++ DebGen.sh
@@ -20,7 +20,7 @@
 ################################################################################
 
 # Set DebGen Version
-VER="v1.0.0"
+VER="v1.0.1"
 
 # Bash/Dash Shell Compatibility
 [ -z "$BASH_VERSION" ] && ECHO="echo" || ECHO="echo -e"
